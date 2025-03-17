b52a134e0f4d798fee66d08d35443482769aa74e
Raghadsami72 <raghadsami850@gmail.com>
2025-03-17 10:57:38 +0300

    Create food_entry.dart

---
+++ lib/models/food_entry.dart
@@ -0,0 +1,11 @@
+class FoodEntry {
+  final String name;
+  final int calories;
+  final DateTime timestamp;
+
+  FoodEntry({
+    required this.name,
+    required this.calories,
+    required this.timestamp,
+  });
+}
